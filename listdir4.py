dc0f5142a631105c33475c39cae795f58e045b82
ehlinger5 <36644419+ehlinger5@users.noreply.github.com>
2018-03-17 22:35:18 -0400

    Update and rename listdir3.py to listdir4.py

--- listdir4.py
+++ listdir4.py
@@ -6,5 +6,5 @@ def files(path):
   for file in os.listdir(path):
     if os.path.isfile(os.path.join(path, file)):
         yield file
-for file in files("."):
+for file in files("/files"):
   print(file)
